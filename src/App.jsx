caca9238c9cb254da449d8c5c85e60069493153b
simidu sodiq <simidusodiqt@gmail.com>
2024-05-13 10:45:59 +0100

    adding note pad

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import writing from "./assets/writing.svg";
+import takeNote from "./assets/Google Keep-10.svg";
+import cancel from "./assets/Google Keep-2.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Sample notes data
+  const categories = [
+    { id: 1, name: "Personal Note", content: "Add notes" },
+    { id: 2, name: "Educational", content: "Add notes" },
+
+    // Add more categories as needed
+  ];
+
+  const hideNote = () => {};
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="heading">
+        <h1>All notes</h1>
+        <img className="writing" src={writing} alt="" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="noteCategories">
+        {categories.map(({ id, name, content }) => {
+          return (
+            <div className="category" key={id}>
+              {" "}
+              {name} <p>{content}</p>
+            </div>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="takeNote">
+        <img src={takeNote} className="svg-icon" />
+      </div>
+
+      <div className="container">
+        <div className="centered-div">
+          <div className="toppart">
+            <h1 className="text">Choose categories</h1>
+            <img src={cancel} className="notePad" />
+          </div>
+
+          <div className="btns">
+            {categories.map(({ id, name }) => (
+              <div key={id}>
+                <button className="btn" type="button">
+                  {name}
+                </button>
+              </div>
+            ))}
+          </div>
+
+          <form className=".form-control">
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Category"
+              className="input input-alt"
+            />
+            <span className="input-border input-border-alt"></span>
+
+            <input type="text" name="" id="" placeholder="Write note here" />
+            <span className="input-border input-border-alt"></span>
+          </form>
+
+          <button type="submit" className="button1">
+            Done
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
